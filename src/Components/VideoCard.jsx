5e7840230c7d6f368a5e9630d0b2da15be9e8007
Bhaweshgoyal <105268824+Bhaweshgoyal@users.noreply.github.com>
2023-03-26 12:32:41 +0530

    new build version

--- src/Components/VideoCard.jsx
+++ src/Components/VideoCard.jsx
@@ -1,24 +1,43 @@
 import { Avatar } from "@mui/material";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../css/VideoCards.css";
-function VideoCard({VideoSrc, image, title, channel, views, timestamp, channelImg }) {
- const [isClicked, setisClicked] = useState(false)
-  function handelClick (){
+function VideoCard({
+  VideoSrc,
+  image,
+  title,
+  channel,
+  views,
+  timestamp,
+  channelImg,
+}) {
+  const [isClicked, setisClicked] = useState(false);
+  function handelClick() {
     setisClicked(true);
   }
 
   return (
     <div className="videoCard" onClick={handelClick}>
-      {isClicked ? <iframe width= {250} height={170} src={`${VideoSrc}`+"?autoplay=1"} allow='autoplay' frameborder="0"></iframe>: <img className="videoCard_thumbnail" src={image} alt="" />}
+      {isClicked ? (
+        <iframe
+          width={250}
+          height={170}
+          src={`${VideoSrc}` + "?autoplay=1"}
+          allow="autoplay"
+          frameborder="0"
+          allowFullScreen
+        ></iframe>
+      ) : (
+        <img className="videoCard_thumbnail" src={image} alt="" />
+      )}
       <div className="video_info">
         <Avatar className="videoCard_avatar" alt={channel} src={channelImg} />
         <div className="videoCard_text">
-        <h4>{title}</h4>
-        <p>{channel}</p>
-        <p>
+          <h4>{title}</h4>
+          <p>{channel}</p>
+          <p>
             {views} &#8226; {timestamp}
-        </p>
-      </div>
+          </p>
+        </div>
       </div>
     </div>
   );
